04773972f1ea159fd9789abe3f98bbbfca578f2d
DingDean <dingkework@hotmail.com>
2018-06-25 22:29:35 +0800

    dev: add server push

--- src/components/sw.vue
+++ src/components/sw.vue
@@ -1,8 +1,10 @@
 <template>
 <div>
   <p v-if='showInstall'>如果你喜欢的话，你可以将这个网页<span class='togglebtn' @click='install'>安装</span>到你的桌面。</p>
-  <p v-if='showSubs'>
+  <p v-if='showSubscription'> 同时，如果你愿意，也可以
     <span class='togglebtn' @click='subscribe'>订阅</span>
+    本网站的通知。我不会滥用你给我的通知权限，所以网站的消息推送会非常克制，现阶段只会在我分享音乐时才会触发通知。
+    推送的频率不会大于2次/月。谢谢你给的信任。
   </p>
 </div>
 </template>
@@ -11,6 +13,13 @@
 import axios from 'axios'
 export default {
   props: ['showInstall', 'showSubs'],
+  computed: {
+    showSubscription () {
+      // chrome fcm is not reachable in mainland China
+      let isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor)
+      return !isChrome && this.showSubs
+    }
+  },
   methods: {
     install () {
       // Show the prompt
@@ -48,7 +57,6 @@ export default {
           return axios.get('/sw/vapidPublicKey')
             .then(function (response) {
               const vapidPublicKey = response.data
-              console.log('get public key: ' + vapidPublicKey)
 
               const convertedVapidKey = self.urlBase64ToUint8Array(vapidPublicKey)
               return reg.pushManager.subscribe({
@@ -57,11 +65,10 @@ export default {
               })
             })
         }).then(function (sub) {
-          console.log('Subscribed', sub.endpoint)
           return axios.post('/sw/register', {
             subscription: sub
           }).then(function (res) {
-            console.log('registered ' + res.data)
+            console.log('registered')
           })
         }).catch(function (e) {
           throw new Error('Failed to subscribe' + e)
